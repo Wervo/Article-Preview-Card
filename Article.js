da0c65f653874aa9f6c4a303cd840276b4fb0aaf
jonah <francisjonah22@gmail.com>
2024-03-06 15:30:43 -0500

    first commit

---
+++ Article.js
@@ -0,0 +1,6 @@
+let social = document.getElementById("socialBtn")
+let socialMedia = document.querySelector(".hidden")
+
+social.addEventListener("click",function(){
+     socialMedia.classList.toggle("hidden");
+})
